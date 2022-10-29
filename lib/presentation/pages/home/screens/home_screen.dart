64dbe66c35067c46bba913a9414dd89d95a3772b
Dimas Saputro <111202012497@mhs.dinus.ac.id>
2022-10-29 19:44:11 +0700

    add carousel

--- lib/presentation/pages/home/screens/home_screen.dart
+++ lib/presentation/pages/home/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:data_utama/shared/styles/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +13,35 @@ class HomeScreen extends StatelessWidget {
         SizedBox(
           height: 32,
         ),
-        
+        Carousel()
       ],
+    );
+  }
+}
+
+class Carousel extends StatelessWidget {
+  const Carousel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(aspectRatio: 547 / 267),
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: Card(
+                elevation: 3,
+                child: Image.asset('assets/carousel_img/img-1.png'),
+              ),
+            );
+          },
+        );
+      }).toList(),
     );
   }
 }
