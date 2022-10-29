abc1baae93b3b6ef46689b7774382a3a970da82b
Dimas Saputro <111202012497@mhs.dinus.ac.id>
2022-10-29 15:14:34 +0700

    add register page route

--- lib/shared/routes.dart
+++ lib/shared/routes.dart
@@ -1,12 +1,18 @@
 import 'package:data_utama/presentation/pages/login_page.dart';
+import 'package:data_utama/presentation/pages/register_page.dart';
 import 'package:data_utama/presentation/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 const String splashPage = "splash-page";
 const String loginPage = "login-page";
+const String registerPage = "register-page";
 
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
+    case registerPage:
+      return MaterialPageRoute(
+        builder: (context) => const RegisterPage(),
+      );
     case loginPage:
       return MaterialPageRoute(
         builder: (context) => const LoginPage(),
